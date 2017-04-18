597961673a3c0e4e4b046b378c135de10c23ad4b
sue martinez <sueherrera30@gmail.com>
2017-04-18 20:44:49 +0000

    ejercicio completito

--- js/main.js
+++ js/main.js
@@ -14,9 +14,33 @@ var botonRestaurar = document.getElementById('restaurando');
 botonRestaurar.addEventListener("click",restaurar);
 
 function restaurar(){
-  for(var i=0;i<tachecito.length;i++){
+  for(var i=0;i<imagenesTotales.length;i++){
 imagenesTotales[i].style.display="inline-block";
+}
+}
+
+var botonOrigen = document.getElementById('elOrigen');
+botonOrigen.addEventListener("click",Origen);
+function Origen(){
+ var origenTexto= document.getElementById("texto1");
+ if(origenTexto.style.display=="none"){
+   origenTexto.style.display="inline-block";
+ }
+ else{
+   origenTexto.style.display= "none";
+ };
 
 }
 
+var botonExtincion =document.getElementById("extinguiedo");
+botonExtincion.addEventListener("click",extincion);
+
+function extincion(){
+  var extincionTexto= document.getElementById("texto2");
+  if(extincionTexto.style.display=="none"){
+    extincionTexto.style.display="inline-block";
+  }
+  else{
+    extincionTexto.style.display= "none";
+  };
 }
