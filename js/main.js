93d798b042bfc3b9e6eca54a2968e615373c3d83
sue martinez <sueherrera30@gmail.com>
2017-04-18 15:56:44 +0000

    version con boton restaurar funcionando

--- js/main.js
+++ js/main.js
@@ -2,9 +2,21 @@
 var imagenesTotales= document.getElementsByClassName("casitaImagen");
 var tachecito=document.getElementsByClassName("tacheCerrar");
 
+
 for(var i=0;i<tachecito.length;i++){
   imagenesTotales[i].addEventListener("click",ocultarImagen);
 }
 function ocultarImagen() {
   this.style.display="none";
+}
+
+var botonRestaurar = document.getElementById('restaurando');
+botonRestaurar.addEventListener("click",restaurar);
+
+function restaurar(){
+  for(var i=0;i<tachecito.length;i++){
+imagenesTotales[i].style.display="inline-block";
+
+}
+
 }
